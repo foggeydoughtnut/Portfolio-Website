2d2ec7cbc4fbbde7bd1a200f296d5fb9c904db09
Jeff <foggeydoughtnut@gmail.com>
2022-05-02 09:40:00 -0600

    added projects to home and projects tab (kind of, need to update for all projects

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,33 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
 import React from "react";
-import '../style.css';
+import '../static/style.css';
+import project from '../static/project.resized.png'
+
 
 const Home = () => {
     return (
-        <div className="center">
+        <div className="center add-padding">
 
             <Typography variant="h2">Welcome!</Typography>
 
             <Typography variant="h5">Current Project</Typography>
-            <Card sx={{ width: '50vw' }}>
-                <CardContent>
-                    <Typography variant='h4'>Current Project</Typography>
-                </CardContent>
-            </Card>
-
-
-
+            <div className="add-padding">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minheight: '100vh' }}
+                >
+                    <Card sx={{ width: '52vw' }}>
+                        <CardContent>
+                            <img src = {project} alt="current-project"/>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </div>
         </div>
-
-
     )
 }
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,97 @@
 import React from "react";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+import project from '../static/project.resized.png'
+import '../static/style.css';
 
 const Projects = () => {
     return (
-        <div>
-            <h1>Projects</h1>
+        <div className="center add-padding">
+            <Typography variant="h2">Projects</Typography>
+
+            <div className="add-padding">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minheight: '100vh' }}
+                >
+                    <Card sx={{ width: '52vw' }}>
+                        <CardContent>
+                            <img src = {project} alt="current-project"/>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </div>
+
+            <div className="add-padding">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minheight: '100vh' }}
+                >
+                    <Card sx={{ width: '52vw' }}>
+                        <CardContent>
+                            <img src = {project} alt="current-project"/>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </div>
+
+            <div className="add-padding">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minheight: '100vh' }}
+                >
+                    <Card sx={{ width: '52vw' }}>
+                        <CardContent>
+                            <img src = {project} alt="current-project"/>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </div>
+
+            <div className="add-padding">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minheight: '100vh' }}
+                >
+                    <Card sx={{ width: '52vw' }}>
+                        <CardContent>
+                            <img src = {project} alt="current-project"/>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </div>
+
+            <div className="add-padding">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minheight: '100vh' }}
+                >
+                    <Card sx={{ width: '52vw' }}>
+                        <CardContent>
+                            <img src = {project} alt="current-project"/>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </div>
         </div>
     )
 }
